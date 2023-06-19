2cbbe525c7038cde5fa17d974250b8af494d6588
jlucasacosta <luacostadelgado@gmail.com>
2023-06-19 19:47:13 -0300

    ajustes

--- js/darkLightMode.js
+++ js/darkLightMode.js
@@ -2,7 +2,7 @@ const btnDark = document.querySelector('#switchDark')
 const btnLight = document.querySelector('#switchLight')
 
 btnDark.onclick = function() { 
-    document.body.className = 'dark__mode'
+    document.body.classList = 'dark__mode'
     btnDark.classList = "switch ball"
     btnLight.classList.remove('ball')
 }
